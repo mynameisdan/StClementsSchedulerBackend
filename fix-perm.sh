a789bdcc0c2a3f8f28dc557e821b07802dece96f
Daniel Moon <dmoon90@gmail.com>
2013-03-29 18:58:24 -0400

    Cleaning repo

--- fix-perm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find $1 -type d | xargs chmod a+rx
-find $1 -type f | xargs chmod a+r
--- web/db/sample-data/demo-bare.sql
+++
@@ -1,4 +0,0 @@
-
--- admin:password
-INSERT INTO ptaweb.admin values ('admin', '5f4dcc3b5aa765d61d8327deb882cf99');
-
